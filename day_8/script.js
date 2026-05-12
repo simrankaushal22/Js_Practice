3d079f36bc0660d60ed0f44f4440e3296c881cf1
Simran Kaushal <simrankaushal374@gmail.com>
2026-05-12 18:29:36 +0530

    first-commit

--- day_8/script.js
+++ day_8/script.js
@@ -54,6 +54,20 @@ for (let i = 1; i <= n; i++) {
 
     console.log(row);
 }
+// upper part
+for (let i = 1; i <= n; i++) {
+    let row = "";
+
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
+    }
+
+    for (let k = 1; k <= (2 * i - 1); k++) {
+        row += "*";
+    }
+
+    console.log(row);
+}
 
 // lower part
 for (let i = n - 1; i >= 1; i--) {
